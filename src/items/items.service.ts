b913269a57e1b1b6c43c1937fcffd35c54977205
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-10 10:37:57 +0900

    Add: dto

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { CreateItemDto } from './dto/create-item.dto'
 import { ItemStatus } from './item-status.enum'
 import { Item } from './item.model'
 
@@ -15,7 +16,11 @@ export class ItemsService {
     return findItem
   }
 
-  create(item: Item): Item {
+  create(createItemDto: CreateItemDto): Item {
+    const item: Item = {
+      ...createItemDto,
+      status: ItemStatus.ON_SALE
+    }
     this.items.push(item)
     return item
   }
