abd70cf738f248a3fa93192ed241efdf7e68812c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-10 10:07:56 +0900

    Add: post methods

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Item } from './item.model'
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private items: Item[] = []
+
+  findAll() {
+    return 'this is itemservice'
+  }
+
+  create(item: Item): Item {
+    this.items.push(item)
+    return item
+  }
+}
