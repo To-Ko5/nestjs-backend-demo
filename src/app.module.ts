1388afe4f7af364e32d6e7b01257bb33800b63a8
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-13 18:33:49 +0900

    Change: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 import { ItemsModule } from './items/items.module'
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot()],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'postgres',
+      autoLoadEntities: true,
+      entities: ['dist/entities/*.entity.js'],
+      migrations: ['dist/migrations/*.js'],
+      cli: {
+        entitiesDir: 'src/entities',
+        migrationsDir: 'src/migrations'
+      }
+    })
+  ],
   controllers: [],
   providers: []
 })
