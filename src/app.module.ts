d5a6aea237283c72686132157901a2b54a2c16d3
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-13 10:50:49 +0900

    Add: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
 import { ItemsModule } from './items/items.module'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'postgres',
+      autoLoadEntities: true
+    })
+  ],
   controllers: [],
   providers: []
 })
