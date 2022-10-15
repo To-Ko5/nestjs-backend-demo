c45ab1e9519bc63ed636889d172188ce3500ef41
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-15 22:26:57 +0900

    Add: signin 機能を追加

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from 'src/entities/user.entity'
 import { Repository } from 'typeorm'
 import { CreateUserDto } from './dto/create-user.dto'
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt'
+import { CredentialsDto } from './dto/credentials.dto'
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService
   ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
@@ -26,7 +29,19 @@ export class AuthService {
     return user
   }
 
-  // async login() {
+  async signIn(
+    credentialsDto: CredentialsDto
+  ): Promise<{ accessToken: string }> {
+    const { userName, password } = credentialsDto
+    const user = await this.userRepository.findOne({ userName })
 
-  // }
+    // jwttokenの作成
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { id: user.id, userName: user.userName }
+      const accessToken = await this.jwtService.sign(payload)
+      return { accessToken }
+    } else {
+      throw new UnauthorizedException('ログインできません')
+    }
+  }
 }
