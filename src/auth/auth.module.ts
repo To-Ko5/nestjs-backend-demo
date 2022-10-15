5a71c5a8972ce15121ead722a5f0499f0f863d5b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-10-15 22:08:29 +0900

    Add: jwtの設定を追加

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { User } from 'src/entities/user.entity'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: 3600 }
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
